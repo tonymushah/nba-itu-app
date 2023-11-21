fca5669b906f7ae7c26b00979d08d2394d71b9b5
tonymushah <tonyramanantsalama@gmail.com>
2023-11-21 10:33:49 +0300

    Initial commit

---
+++ nba/src/main/java/mg/tonymushah/itu/nba/NbaApplication.java
@@ -0,0 +1,13 @@
+package mg.tonymushah.itu.nba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaApplication.class, args);
+	}
+
+}
